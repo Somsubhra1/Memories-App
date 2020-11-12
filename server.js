5b262f9cb587a41c3215d96d2c95d2f882a76121
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-12 21:48:50 +0530

    db setup

--- server.js
+++ server.js
@@ -2,3 +2,30 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL = process.env.MongoURI;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Successfully connected to MongoDB"))
+  .catch((err) => console.log(`Error connecting to MongoDB ${err.message}`));
+
+app.get("/", (req, res) => res.send("Hello"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
